e422d61542b6e79fe9e7c33cb5f0af0b366ccaad
meme018 <anjalixunar9@gmail.com>
2026-04-08 22:36:28 +0545

    origin fix

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const {
 } = require("./controller/blogController");
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://furniture-ecom-iota.vercel.app/"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
