2d0d5bfb1950f1d153cf24297e89726e3593cfce
meme018 <anjalixunar9@gmail.com>
2026-04-08 22:01:37 +0545

    backend cors origin

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const {
 } = require("./controller/blogController");
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
